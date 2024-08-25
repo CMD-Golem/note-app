30b019ecd528557511a4e257874e7e9d874f0564
CMD-Golem <64982383+CMD-Golem@users.noreply.github.com>
2024-08-25 21:32:22 +0200

    test blob run 6

---
+++ web/netlify/functions/blob_handler.js
@@ -0,0 +1,32 @@
+import { connectLambda, getStore } from "@netlify/blobs";
+
+exports.handler = async (event) => {
+	connectLambda(event)
+
+	if (event.httpMethod == "GET") {
+		var {store, path, type} = JSON.parse(event.body);
+		var response = await getStore(store).get(path, {type:type});
+	}
+	else if (event.httpMethod !== "POST") {
+		var {store, path, type, data} = JSON.parse(event.body);
+		if (type == "base64") {
+			var base64 = data.replace(/^data:image\/\w+;base64,/, "");
+			var buffer = Buffer.from(base64, "base64");
+
+			var response = await getStore(store).set(path, buffer);
+		}
+		else var response = await getStore(store).set(path, data);
+	}
+	else if (event.httpMethod !== "DELETE") {
+		var {store, path} = JSON.parse(event.body);
+		var response = await getStore(store).get(path);
+	}
+	else {
+		var response = {statusCode:405, body:"Method Not Allowed"};
+	}
+
+	return {
+		statusCode: 200,
+		body: JSON.stringify(response)
+	}
+};
