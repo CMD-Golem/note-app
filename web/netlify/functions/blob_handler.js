1a4fb1f50e2baa6ce5d3433773f0328ab129fcda
CMD-Golem <64982383+CMD-Golem@users.noreply.github.com>
2024-09-01 17:52:14 +0200

    test blob run 13

--- web/netlify/functions/blob_handler.js
+++ web/netlify/functions/blob_handler.js
@@ -3,24 +3,43 @@ import { connectLambda, getStore } from "@netlify/blobs";
 exports.handler = async (event) => {
 	connectLambda(event)
 
+	// Get data
 	if (event.httpMethod == "POST") {
-		var {store, path, type} = JSON.parse(event.body);
-		var response = await getStore(store).get(path, {type:type});
+		var {store, path} = JSON.parse(event.body);
+		var data = await getStore(store).get(path, {type:"arrayBuffer"});
+		var array_buffer = await data.arrayBuffer();
+
+		return {
+			statusCode: 200,
+			body: JSON.stringify(Array.from(new Uint8Array(array_buffer)))
+		}
 	}
+	// upload data
 	else if (event.httpMethod == "PUT") {
 		var {store, path, data} = JSON.parse(event.body);
-		var response = await getStore(store).set(path, data);
+
+		var buffer = new Uint8Array(data).buffer;
+
+		var response = await getStore(store).set(path, buffer);
+
+		return {
+			statusCode: 200,
+			body: JSON.stringify(response)
+		}
 	}
+	// delete data
 	else if (event.httpMethod == "DELETE") {
 		var {store, path} = JSON.parse(event.body);
 		var response = await getStore(store).delete(path);
+
+		return {
+			statusCode: 200,
+			body: JSON.stringify(response)
+		}
 	}
 	else {
-		var response = {statusCode:405, body:"Method Not Allowed"};
-	}
-
-	return {
-		statusCode: 200,
-		body: JSON.stringify(response)
+		return {statusCode:405, body:"Method Not Allowed"};
 	}
 };
+
+
