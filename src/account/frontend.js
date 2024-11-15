36244a6e823f5634a79948363fefd9b58d1dc939
CMD-Golem <64982383+CMD-Golem@users.noreply.github.com>
2024-11-15 23:51:44 +0100

    login page + new encryption

--- src/account/frontend.js
+++ src/account/frontend.js
@@ -1,4 +1,8 @@
 export default class Account {
+	static active_type;
+	static uuid_list;
+	static type_dictionary = {login:"Login", register:"Register", generate:"Generate", reset_pw:"Reset Password", logout:"Logout"};
+
 	static async load() {
 		var main = document.querySelector("main");
 		// button.addEventListener("click", app.buttonClick.bind(null, true));
@@ -17,7 +21,7 @@ export default class Account {
 		// 			secret: user_data.secret,
 		// 			device_id: user_data.id,
 		// 			request_data: {
-		// 				iv: user_data.iv,
+		// 				pdk: user_data.pdk,
 		// 				encrypted: true,
 		// 				paths: ["account.json.enc"]
 		// 			}
@@ -28,24 +32,34 @@ export default class Account {
 		// 	var decoder = new TextDecoder("utf-8");
 		// 	var json = decoder.decode(new Uint8Array(result.arrays[0]));
 		// 	var account_data = JSON.parse(json);
+
+		// Public.uuid_list = account_data.
 		// }
+
+		document.querySelector("main").setAttribute("data-loggedin", true);
+		document.querySelector("main").setAttribute("data-loggedin", false);
 
 
 		main.innerHTML = `
 			<div class="selection">
-				<button id="login" onclick="app.changeButtons('login')">Login</button>
-				<button id="register" onclick="app.changeButtons('register')">Register</button>
-				<button id="generate" onclick="app.changeButtons('generate')">Generate</button>
+				<button id="login" class="unkown_user" onclick="app.changeButtons('login')">Login</button>
+				<button id="register" class="unkown_user" onclick="app.changeButtons('register')">Register</button>
+				<button id="generate" class="unkown_user know_user" onclick="app.changeButtons('generate')">Generate</button>
+				<button id="reset_pw" class="know_user" onclick="app.changeButtons('reset_pw')">Reset Login</button>
+				<button id="logout" class="know_user" onclick="app.changeButtons('logout')">Logout</button>
 			</div>
 
 
 			<input placeholder="User Name" id="login_user" autocomplete="username" class="login register">
-			<input placeholder="Page" id="login_page" autocomplete="one-time-code" class="generate">
+			<input placeholder="UUID" id="login_uuid" autocomplete="one-time-code" class="generate" list="uuids">
 			<input placeholder="Password" id="login_password" type="password" autocomplete="current-password" class="login register generate">
-			<input placeholder="Confirm Password" id="login_confirm" type="password" autocomplete="one-time-code" class="register">
+			<input placeholder="Old Password" id="login_old" type="password" autocomplete="current-password" class="reset_pw">
+			<input placeholder="New Password" id="login_new" type="password" autocomplete="current-password" class="reset_pw">
+			<input placeholder="Confirm Password" id="login_confirm" type="password" autocomplete="one-time-code" class="register reset_pw">
 			<input placeholder="Identifier" id="login_identifier" type="password" autocomplete="one-time-code" class="generate">
-			<button id="main_button" onlick="app.buttonClick()"></button>
+			<button id="main_button" onclick="app.buttonClick()"></button>
 			<div id="login_info"></div>
+			<datalist id="uuids"></datalist>
 		`;
 		
 		Public.changeButtons("generate");
@@ -53,21 +67,22 @@ export default class Account {
 		console.log("loaded Account");
 	}
 
-	static async login(create_account) {
-		var user = document.getElementById("login_user").value;
-		var password = document.getElementById("login_password").value;
-	
-		if (create_account) var method = "PUT";
-		else var method = "POST";
-	
+	static async account(action, user, password, data) {	
+		console.log(data)
+
 		var response = await fetch("/.netlify/functions/user", {
-			method: method,
-			headers: {"Content-Type": "application/json"},
-			body: JSON.stringify({
-				user: user,
-				password: password
-			})
+			method: "POST",
+			headers: JSON.stringify({
+				"Content-Type": "application/json",
+				Authorization: {
+					user: user,
+					password: password
+				},
+				Action: action
+			}),
+			body: JSON.stringify(data)
 		});
+
 		if (response.status != 200) {
 			console.log(response.text());
 			return
@@ -75,7 +90,7 @@ export default class Account {
 	
 		var user_data = await response.json();
 
-		user_data.iv = await Account.generateIv(password, user, user_data.salt, 32);
+		user_data.pdk = await Account.generatePdk(password, user, user_data.salt, 32);
 		user_data.user = user;
 		delete user_data.salt;
 	
@@ -83,15 +98,60 @@ export default class Account {
 		console.log(user_data);
 	}
 
-	static async showIv() {
+	static async login() {
 		var user = document.getElementById("login_user").value;
+		var password = document.getElementById("login_password").value;
+	
+		if (user.length == 0 || password.length == 0) return "Inputs must be filled out";
+		else return await Account.account("login", user, password, undefined);
+	}
+
+	static async register() {
+		var user = document.getElementById("login_user").value;
+		var password = document.getElementById("login_password").value;
+		var login_confirm = document.getElementById("login_confirm").value;
+	
+		if (user.length == 0 || password.length == 0 || login_confirm.length == 0) return "Inputs must be filled out";
+		else if (password != login_confirm) return "Passwords don't match";
+		else {
+			var data = {};
+			data.salt = Account.generateSalt();
+			data.pdk = await Account.generatePdk(password, user, data.salt, 32);
+			return await Account.account("register", user, password, data);
+		}
+	}
+
+	static async resetPassword() {
+		var login_old = document.getElementById("login_user").value;
+		var login_new = document.getElementById("login_password").value;
+		var login_confirm = document.getElementById("login_confirm").value;
+
+		var user_data = JSON.parse(localStorage.getItem("user"));
+	
+		if (user_data == null) return "Log in first";
+		else if (login_old.length == 0 || login_new.length == 0 || login_confirm.length == 0) return "Inputs must be filled out";
+		else if (login_new != login_confirm) return "Passwords don't match";
+		else  {
+			var data = {};
+			data.new = login_new;
+			data.old_pdk = user_data.pdk;
+			data.secret = user_data.secret;
+			data.id = user_data.id;
+			data.salt = Account.generateSalt();
+			data.new_pdk = await Account.generatePdk(login_new, user_data.user, data.salt, 32);
+			return await Account.account("reset_password", user_data.user, login_old, data);
+		}
+	}
+
+	static async showPdk() {
+		var user = document.getElementById("login_uuid").value;
 		var password = document.getElementById("login_password").value;
 		var identifier = document.getElementById("login_identifier").value;
 
-		var iv = generateIv(password, user, identifier, length);
+		return await Account.generatePdk(password, user, identifier, 20);
 	}
 
-	static async generateIv(masterKey, domain, masterString, string) {
+	static async generatePdk(masterKey, domain, masterString, string) {
 		var encoder = new TextEncoder();
 		var crypto_key = await crypto.subtle.importKey(
 			"raw",
@@ -101,33 +161,58 @@ export default class Account {
 			["sign"]
 		);
 	
-		var iv_string = "";
+		var pdk_string = "";
 		var charset = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!?@#$%^&*()_+-=[]{}|/;:,.<>";
 		var signature = await crypto.subtle.sign("HMAC", crypto_key, encoder.encode(domain + masterString));
-		var iv_array = Array.from(new Uint8Array(signature));
+		var pdk_array = Array.from(new Uint8Array(signature));
+
+		console.log(signature)
 		
-		for (let i = 0; i < iv_array.length; i++) {
-			iv_string += charset[iv_array[i] % charset.length];
+		for (let i = 0; i < pdk_array.length; i++) {
+			pdk_string += charset[pdk_array[i] % charset.length];
 		}
 
-		return iv_string.slice(0, string);
+		return pdk_string.slice(0, string);
+	}
+
+	static generateSalt() {
+		var salt_array = crypto.getRandomValues(new Uint8Array(64));
+		return btoa(String.fromCharCode.apply(null, salt_array));
 	}
 }
 
 export class Public {
-	static active_type;
-	static type_dictionary = {login:"Login", register:"Register", generate:"Generate"};
-
 	static async buttonClick() {
-		// check if device already
-		if (active_type == "login") Account.login(false);
-		else if (active_type == "register") Account.login(true);
-		
+		document.getElementById("login_info").innerHTML = "";
+
+		switch (Account.active_type) {
+			case "login":
+				var response = await Account.login();
+				break;
+			case "register":
+				var response = await Account.register();
+				break;
+			case "generate": 
+				var response = await Account.showPdk();
+				break;
+			case "reset_pw":
+				var response = await Account.resetPassword();
+				break;
+			case "logout":
+				localStorage.removeItem("user");
+				// location.reload();
+				break;
+		}
+
+		document.getElementById("login_info").innerHTML = response || "";
+		// clear inputs
 	}
 
 	static changeButtons(type) {
-		this.active_type = type;
-		document.getElementById("main_button").innerHTML = this.type_dictionary[type];
+		Account.active_type = type;
+		document.getElementById("main_button").innerHTML = Account.type_dictionary[type];
 		document.querySelector("main").setAttribute("data-type", type);
+		document.getElementById("login_info").innerHTML = "";
+		// clear inputs
 	}
 }
--- web/netlify/functions/user.js
+++ web/netlify/functions/user.js
@@ -14,26 +14,27 @@ import crypto from "crypto";
 exports.handler = async (event) => {
 	connectLambda(event);
 
-	var {user, password} = JSON.parse(event.body);
-	var data = await getStore(user).get("user.json", {type:"json", consistency:"strong"});
+	var {Authorization, Action} = JSON.parse(event.header);
+	var data = await getStore(Authorization.user).get("user.json", {type:"json", consistency:"strong"});
 
-	// check login attempt
-	if (event.httpMethod == "POST") {
+	if (Action == "login") {
 		// check if user exists
 		if (data == null) return {
-			statusCode: 404,
-			body: "This user doesn't exitsts"
+			statusCode: 406,
+			body: "Incorrect user or password"
 		}
 
 		// check password and send new device_id, secret
-		var correct_password = await bcrypt.compare(password, data.password_hash)
+		var correct_password = await bcrypt.compare(Authorization.password, data.password_hash);
 		if (correct_password) {
+
+			// device identification
 			var new_device = crypto.randomBytes(32).toString("hex");
 			var secret = crypto.randomBytes(32).toString("hex");
 			var time = new Date().toISOString();
 			data.devices.push({ id:new_device, secret:secret, timestamp:time });
 
-			var response = await getStore(user).setJSON("user.json", data);
+			var response = await getStore(Authorization.user).setJSON("user.json", data);
 			return  {
 				statusCode: 200,
 				headers: {"Content-Type": "application/json"},
@@ -49,12 +50,12 @@ exports.handler = async (event) => {
 		else {
 			return {
 				statusCode: 406,
-				body: "User or Password is wrong"
+				body: "Incorrect user or password"
 			}
 		}
 	}
 	// new user
-	else if (event.httpMethod == "PUT") {
+	else if (Action == "register") {
 		// check if username is already taken
 		if (data != null) return {
 			statusCode: 409,
@@ -62,19 +63,35 @@ exports.handler = async (event) => {
 		}
 
 		// create new user
+		var hash = await bcrypt.hash(Authorization.password, 10); // password hash
+
+		// device identification
 		var device_id = crypto.randomBytes(32).toString("hex");
 		var secret = crypto.randomBytes(32).toString("hex");
-		var salt = crypto.randomBytes(32).toString("hex");
-		var hash = await bcrypt.hash(password, 10);
-		var key = await crypto.webcrypto.subtle.generateKey( {name: "AES-GCM", length: 256}, true, ["encrypt", "decrypt"] );
-		var exported_key = await crypto.webcrypto.subtle.exportKey('jwk', key);
 		var time = new Date().toISOString();
 
-		var response = await getStore(user).setJSON("user.json", {
-			user_name: user,
+		// generate random values and store mek
+		var encoder = new TextEncoder();
+
+		var iv = crypto.randomBytes(32).toString("hex"); // iv to encrypt mek
+		var mek = crypto.randomBytes(32).toString("hex"); // Master Encryption Key (MEK)
+		var pdk_key = await crypto.subtle.importKey('raw', encoder.encode(request_data.pdk), { name: 'AES-GCM' }, false, ['encrypt']);  // Password-Derived Key (PDK)
+		var mek_key = await crypto.subtle.encrypt({name: 'AES-GCM', iv: iv }, pdk_key, encoder.encode(mek));
+
+		// encryption key
+		var key = await crypto.webcrypto.subtle.generateKey( {name: "AES-GCM", length: 256}, true, ["encrypt", "decrypt"] );
+		var exported_key = await crypto.webcrypto.subtle.exportKey('jwk', key);
+		
+		// store data
+		var request_data = JSON.parse(event.body);
+
+		var response = await getStore(Authorization.user).setJSON("user.json", {
+			user_name: Authorization.user,
 			password_hash: hash,
 			encryption_key: exported_key,
-			salt: salt,
+			iv: iv,
+			mek: new Uint8Array(mek_key),
+			salt: request_data.salt,
 			devices: [{ id:device_id, secret:secret, timestamp:time }]
 		});
 
@@ -87,6 +104,51 @@ exports.handler = async (event) => {
 				secret: secret,
 				salt: salt
 			})
+		}
+	}
+	else if (Action == "reset_pw") {
+		// check if user exists
+		if (data == null) return {
+			statusCode: 406,
+			body: "Incorrect user or password"
+		}
+
+		// check password and reset password
+		var correct_password = await bcrypt.compare(Authorization.password, data.password_hash);
+		if (correct_password) {
+			var request_data = JSON.parse(event.body);
+			var encoder = new TextEncoder();
+
+			data.password_hash = await bcrypt.hash(request_data.new, 10); // password hash
+			data.salt = request_data.salt;
+
+			// decrypt mek with old pdk
+			var old_pdk = await crypto.subtle.importKey('raw', encoder.encode(request_data.old_pdk), { name: 'AES-GCM' }, false, ['decrypt']);  // get current pdk
+			var mek_key = await crypto.subtle.decrypt({ name: 'AES-GCM', iv: data.iv }, old_pdk, data.mek); // decrypt mek
+
+			// decrypt mek with new pdk and store everything
+			data.iv = crypto.randomBytes(32).toString("hex"); // iv to encrypt mek
+			var new_pdk = await crypto.subtle.importKey('raw', encoder.encode(request_data.new_pdk), { name: 'AES-GCM' }, false, ['encrypt']);
+			var mek = await crypto.subtle.encrypt({name: 'AES-GCM', iv: data.iv }, new_pdk, mek_key);
+			data.mek = new Uint8Array(mek);
+
+			var response = await getStore(Authorization.user).setJSON("user.json", data);
+			return  {
+				statusCode: 200,
+				headers: {"Content-Type": "application/json"},
+				body: JSON.stringify({
+					response: response,
+					id: request_data.id,
+					secret: request_data.secret,
+					salt: data.salt
+				})
+			}
+		}
+		else {
+			return {
+				statusCode: 406,
+				body: "Incorrect user or password"
+			}
 		}
 	}
 	else {
